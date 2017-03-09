0bc861d44ff89640f91860444142b2cbaa6f93c9
bobbybakes <bobby.a.baker@gmail.com>
2017-03-09 17:02:38 -0500

    succesfully added markers to am image in relation to the actual image

--- client/main.js
+++ client/main.js
@@ -5,13 +5,12 @@ import './main.html';
 
 Template.map.rendered = function () {
     console.log('-------initializing map---------')
-    map = L.map('map').setView([-70, -70], 4);
-    L.tileLayer('http://localhost:8000/sampleTrack/{z}/{x}/{y}.png', {
-        minZoom: 1,
-        maxZoom: 6,
-        tms: true
-    }).addTo(map);
-
+    map = L.map('map', {
+        crs: L.CRS.Simple
+    }).setView([500, 500], 0);
+    var bounds = [[0, 0], [1000, 1000]];
+    map.fitBounds(bounds);
+    var image = L.imageOverlay('/img/fakeTRCd1000.png', bounds).addTo(map);
 
     let cameraIcon = L.icon({
         iconUrl: 'http://www.clipartbest.com/cliparts/KTj/o54/KTjo548ac.png',
@@ -22,14 +21,14 @@ Template.map.rendered = function () {
         shadowAnchor: [22, 94]
     });
 
-    let camera1 = new L.Marker([-80, -70], {
+    let camera1 = new L.Marker([500, 500], {
         icon: cameraIcon
     }).on('click', function (e) {
         $(".camera").toggle();
     }).addTo(map);
     Locations.find({}).observe({
         changed: function (car) {
-            console.log(car.id + '\'s location has changed to X:' + car.x  + " Y:" + car.y);
+            console.log(car.id + '\'s location has changed to X:' + car.x + " Y:" + car.y);
             map.removeLayer(markers[car.id]);
             delete markers[car.id];
             var marker = new L.marker([car.x, car.y], {
@@ -55,11 +54,7 @@ Template.map.rendered = function () {
 var createIcon = function () {
     return L.icon({
         iconUrl: 'http://www.clker.com/cliparts/q/0/m/g/P/c/red-sports-car-top-view.svg',
-        iconSize: [100, 50],
-        iconAnchor: [22, 94],
-        popupAnchor: [-3, -76],
-        shadowSize: [68, 95],
-        shadowAnchor: [22, 94]
+        iconSize: [30, 15],
     })
 };
 
