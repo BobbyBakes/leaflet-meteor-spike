c5e33b3eeb3fb721a65406f70126392efdd9f42d
bobbybakes <bobby.a.baker@gmail.com>
2017-03-10 14:32:35 -0500

    updated markers

--- client/main.js
+++ client/main.js
@@ -4,28 +4,32 @@ import {
 import './main.html';
 
 Template.map.rendered = function () {
-    console.log('-------initializing map---------')
+    console.log('-------Initializing Map---------')
     map = L.map('map', {
         crs: L.CRS.Simple
-    }).setView([500, 500], 0);
-    var bounds = [[0, 0], [1000, 1000]];
-    map.fitBounds(bounds);
-    var image = L.imageOverlay('/img/fakeTRCd1000.png', bounds).addTo(map);
-
-    let cameraIcon = L.icon({
-        iconUrl: 'http://www.clipartbest.com/cliparts/KTj/o54/KTjo548ac.png',
-        iconSize: [50, 40],
-        iconAnchor: [22, 94],
-        popupAnchor: [-3, -76],
-        shadowSize: [68, 95],
-        shadowAnchor: [22, 94]
+    }).setView([500, 500], 1).on('zoomend', function () {
+        let currentZoom = map.getZoom();
+        _.each(markers, function (marker) {
+            console.log('Setting markers radius as: ' + currentZoom);
+            marker.radius = currentZoom;
+        })
     });
 
+    var bounds = [[0, 0], [1000, 1000]];
+    map.fitBounds(bounds);
+    var image = L.imageOverlay('img/TRC-Map_web.svg', bounds).addTo(map);
+
+    let purpleCamera = 'img/cameraPin-Purple.png';
+    let cameraIcon = L.icon({
+        iconUrl: purpleCamera,
+        iconSize: [128 / 3, 126 / 3],
+    });
     let camera1 = new L.Marker([500, 500], {
         icon: cameraIcon
     }).on('click', function (e) {
         $(".camera").toggle();
     }).addTo(map);
+
     Locations.find({}).observe({
         changed: function (car) {
             console.log(car.id + '\'s location has changed to X:' + car.x + " Y:" + car.y);
@@ -33,16 +37,16 @@ Template.map.rendered = function () {
             delete markers[car.id];
             var marker = new L.marker([car.x, car.y], {
                 _id: car.id,
-                icon: createIcon()
+                icon: createIcon(car)
             });
             markers[car.id] = marker;
             map.addLayer(marker);
         },
         added: function (car) {
-            console.log('adding ' + car.id + '\'s location');
+            console.log('Adding ' + car.id + '\'s location');
             var marker = new L.marker([car.x, car.y], {
                 _id: car.id,
-                icon: createIcon()
+                icon: createIcon(car)
             });
             markers[car.id] = marker;
             map.addLayer(marker);
@@ -51,10 +55,10 @@ Template.map.rendered = function () {
 };
 
 
-var createIcon = function () {
+var createIcon = function (car) {
     return L.icon({
-        iconUrl: 'http://www.clker.com/cliparts/q/0/m/g/P/c/red-sports-car-top-view.svg',
-        iconSize: [30, 15],
+        iconUrl: car.icon,
+        iconSize: [86 / 3, 89 / 3],
     })
 };
 
